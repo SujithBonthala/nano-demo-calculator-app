2533b7879186360f4e27771909822f08e73412a7
Sujith Bonthala <sujithbonthala7@gmail.com>
2023-08-19 07:34:17 +0530

    Fourth commit

--- routes.cpp
+++ routes.cpp
@@ -1,5 +1,6 @@
 #include "crow_all.h"
 #include <bits/stdc++.h>
+using namespace std;
 
 crow::response greet()
 {
@@ -9,13 +10,13 @@ crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
     crow::json::wvalue res;
-    res['result'] = req.body.first + req.body.second;
+    res["result"] = req.body["first"] + req.body["second"];
     return crow::response(move(res));
 }
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
     crow::json::wvalue res;
-    res['result'] = req.body.first - req.body.second;
+    res["result"] = req.body["first"] - req.body["second"];
     return crow::response(move(res));
 }
